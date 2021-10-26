d2c9202ec14979f87573e5d88064c4f7fdf35a6e
yuulin <29588233+yuulin@users.noreply.github.com>
2021-10-26 14:07:43 +0800

    Initial commit

---
+++ src/compiler/index.ts
@@ -0,0 +1,204 @@
+import { u } from 'unist-builder';
+import { cloneDeep, get, random, sample } from 'lodash';
+import { Paragraph, Sentence, TextNode, Word } from 'nlcst-types';
+import { visit } from 'unist-util-visit';
+import { Root, Content, toString } from 'nlcst-to-string';
+import { ITemplateData } from 'src/parser';
+import { EmptyTemplateTextError, NoResourceForOutlineError, NoValueForSlotError } from './errors';
+import type { JSONSchema7 } from 'json-schema';
+
+export interface IConfiguration {
+  sub?: Record<string, string>;
+}
+export const emptyConfigurationString = '{ "sub": {} }';
+
+/**
+ * 从模板里定义的多个大纲里随机抽取一个出来用
+ */
+export function getRandomOutline(outlines: ITemplateData['outlines'] | undefined): string[] {
+  return sample(outlines ?? []) ?? [];
+}
+
+/**
+ * 将解析出的模板结构化数据转换为 NLCST Root 节点
+ * Paragraph > Sentence > Word > Text
+ *
+ * Paragraph：大纲每行生成的结果；
+ * Sentence：模板库里的内容单位，每个对应模板里一个自然段
+ * Word-Text：具体的模板内容，每个对应模板里一行
+ */
+export function getRandomArticle(template: ITemplateData, config: IConfiguration): Root {
+  const paragraphs = getRandomOutline(template.outlines).map((outlineLine) => {
+    // 根据大纲获取随机素材
+    const dataPath = outlineLine.split('：').join('.');
+    const paragraph = get(template.resources, dataPath);
+    if (paragraph?.type !== 'ParagraphNode') {
+      throw new NoResourceForOutlineError(outlineLine);
+    }
+    // 开始随机化素材内容
+    const randomizedParagraph = u('ParagraphNode', { children: [] as Sentence[] });
+    randomizedParagraph.children = (paragraph as Paragraph).children.map((sentence) => {
+      if (sentence?.type !== 'SentenceNode' || (sentence as Sentence).children.length === 0) {
+        throw new EmptyTemplateTextError(outlineLine);
+      }
+      // 随机挑选自然段里的某一行内容，即 Sentence 里的一个 Word，并对树做转换，
+      const originalSentence = sentence as Sentence;
+      const randomizedSentence = u('SentenceNode', { children: [] as Word[] });
+      const randomWordIndex = random(originalSentence.children.length - 1);
+      // 执行模板替换，需要先 cloneDeep 原先的叶子节点，不然模板内容替换操作会影响原有模板
+      const randomWord = cloneDeep(originalSentence.children[randomWordIndex]) as Word;
+      randomizedSentence.children.push(randomWord);
+      return randomizedSentence;
+    });
+    return randomizedParagraph;
+  });
+
+  const article = u('RootNode', { children: paragraphs }) as Root;
+  return article;
+}
+
+/**
+ * 收集模板数据中的槽位信息
+ */
+export function collectSlots(template: ITemplateData): string[] {
+  const slots = new Set<string>();
+  template.outlines?.forEach((outline) =>
+    outline.forEach((outlineLine) => {
+      // 根据大纲获取随机素材
+      const dataPath = outlineLine.split('：').join('.');
+      const paragraph = get(template.resources, dataPath);
+      // 把具体内容填入槽中
+      if (paragraph !== undefined && paragraph.type === 'ParagraphNode') {
+        visit(paragraph as Paragraph, 'TextNode', (textNode: TextNode) => {
+          // 看看是否需要替换槽位
+          if (typeof textNode.slot === 'string') {
+            slots.add(textNode.slot);
+          }
+        });
+      }
+    }),
+  );
+  return [...slots.keys()];
+}
+
+/**
+ * 为 UI 制作待填参数表单
+ */
+export function getConfigSchemaFromTemplate(templateData: ITemplateData): JSONSchema7 {
+  const slots = collectSlots(templateData);
+  const configSchemaBase: JSONSchema7 = {
+    title: '模板参数',
+    description: '生成流程所需的参数',
+    type: 'object',
+    required: [],
+    properties: {
+      sub: {
+        title: '模板槽位',
+        description: '填入{{槽位}}中的内容',
+        type: 'object',
+        required: [],
+        properties: {},
+      },
+    },
+  };
+  for (const slot of slots) {
+    (configSchemaBase.properties!.sub as JSONSchema7).properties![slot] = {
+      type: 'string',
+      title: slot,
+    };
+  }
+  return configSchemaBase;
+}
+
+/**
+ * 替换 NLCST Root 节点中的所有 Text 节点中的模板槽位
+ */
+export function replaceSlotValues(article: Root, config: IConfiguration): void {
+  // 把具体内容填入槽中
+  visit(article, 'TextNode', (textNode: TextNode) => {
+    // 看看是否需要替换槽位
+    if (typeof textNode.slot === 'string') {
+      const actuarialValueForSlot = config.sub?.[textNode.slot];
+      if (actuarialValueForSlot === undefined) {
+        throw new NoValueForSlotError(textNode.slot);
+      }
+      textNode.value = actuarialValueForSlot;
+      delete textNode.slot;
+    }
+  });
+}
+
+/**
+ * 提取 NLCST Root 节点中的所有 Text 节点中的元信息，放到其上级 Word 中
+ */
+export function extractMetadataValues(article: Root, config: IConfiguration): void {
+  // 把元信息移除
+  visit(article, 'WordNode', (wordNode: Word) => {
+    if (Array.isArray(wordNode.children)) {
+      wordNode.children.forEach((textNode) => {
+        if (Array.isArray(textNode.metadata)) {
+          // 如果不慎存在多个元信息语法，则最后一个会覆盖之前的
+          wordNode.data = { ...wordNode.data, metadata: textNode.metadata };
+        }
+      });
+    }
+  });
+}
+/**
+ * 删除 NLCST Root 节点中的所有 Text 节点中的元信息
+ */
+export function removeMetadataValues(article: Root, config: IConfiguration): void {
+  // 把元信息移除
+  visit(article, 'TextNode', (textNode: TextNode) => {
+    if (Array.isArray(textNode.metadata)) {
+      textNode.value = '';
+      delete textNode.metadata;
+    }
+  });
+}
+
+/**
+ * 将结构化的模板数据变成字符串
+ */
+export function randomOutlineToStringCompiler(template: ITemplateData, config: IConfiguration): string {
+  const article = getRandomArticle(template, config);
+  replaceSlotValues(article, config);
+  removeMetadataValues(article, config);
+  // 成文
+  return toString(article);
+}
+
+/**
+ * 将结构化的模板数据变成根据 Word 拆分的字符串的数组，也就是模板里的一行对应数组里的一个字符串
+ */
+export function randomOutlineToArrayCompiler(template: ITemplateData, config: IConfiguration): string[] {
+  const article = getRandomArticle(template, config);
+  replaceSlotValues(article, config);
+  removeMetadataValues(article, config);
+  // 获取 Word 列表
+  const words = article.children.flatMap((paragraph) => (paragraph as Paragraph).children.flatMap((sentence) => (sentence as Sentence).children)) as Content[];
+  return words.map((word) => toString(word));
+}
+
+export interface IOutputWIthMetadata<T extends unknown[]> {
+  metadata: T | undefined;
+  value: string;
+}
+/**
+ * 将结构化的模板数据变成根据 Word 拆分的字符串的数组，也就是模板里的一行对应数组里的一个字符串，而且带上元信息
+ */
+export function randomOutlineToArrayWithMetadataCompiler<T extends unknown[]>(template: ITemplateData, config: IConfiguration): Array<IOutputWIthMetadata<T>> {
+  const article = getRandomArticle(template, config);
+  replaceSlotValues(article, config);
+  extractMetadataValues(article, config);
+  removeMetadataValues(article, config);
+  // 获取 Word 列表
+  const wordsWithMetadata = article.children.flatMap((paragraph) =>
+    (paragraph as Paragraph).children.flatMap((sentence) =>
+      (sentence as Sentence).children.map((word) => {
+        return [word, word?.data?.metadata as T | undefined];
+      }),
+    ),
+  ) as Array<[Content, T]>;
+  return wordsWithMetadata.map((wordWithMetadata) => ({ value: toString(wordWithMetadata[0]), metadata: wordWithMetadata[1] }));
+}
---
+++ src/parser/index.ts
@@ -0,0 +1,185 @@
+import { get, set } from 'lodash';
+import { VFile } from 'vfile';
+import { TextNode, Sentence, Paragraph, Word } from 'nlcst-types';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import type { Content } from 'mdast-util-from-markdown/lib';
+import { u } from 'unist-builder';
+import { NoOutlineInTemplateError, NoBigTitleInTemplateError, NoTitleContentError, BadTextBetweenTitleError, NoTextBetweenTitleError } from './errors';
+
+/**
+ * 模板资源的树状结构
+ * ```json
+ * {
+ *    "做得好的": {
+ *      "项目·起": Paragraph<Sentence[]> // [["在项目发展早期", "{{xxx}}"], ["3333"]]
+ *    }
+ * }
+ * ```
+ */
+export type ITemplateResource = Record<string, Paragraph | undefined | ITemplateResourceValue>;
+interface ITemplateResourceValue extends ITemplateResource {}
+
+export interface ITemplateData {
+  /**
+   * 使用模板内容的大纲，每一行相当于对指定标题的引用。可以有多个大纲，生成时将随机抽取一个大纲来生成，然后从引用的标题里随机抽取文本。
+   */
+  outlines?: string[][];
+  resources: ITemplateResource;
+  title: string;
+}
+
+/**
+ * 通过认真领会精神，把一个包含学习资料的字符串变为 UNIST（此处为 NLCST）节点，并保留待填入具体内容的槽，成为一个模板 CST
+ * @param templateFile 将会在里面加入一些报错信息，可以通过 vfile-reporter 读取
+ *
+ * // TODO: Word 节点按定义应该是词级别，但是因为我们将大段模板文本看做整体（模因位点），故本次直接在 Word 里面带上含有大段文本的 Text，把 Word 当 Phrase 来看待。未来应该完全使用带词性标注的词级节点，并在使用侧增加利用方法
+ */
+export function templateFileToNLCSTNodes(templateFile: VFile): ITemplateData {
+  // 我们使用的 Markdown 的标题结构是标准 Markdown 的子集，因此可以用 UNIST 社区提供的 MDAST 工具提取出所有信息
+  const mdastInstance = fromMarkdown(templateFile.value);
+  // 先进行一些预发检测
+  if (mdastInstance.children[0]?.type !== 'heading' || mdastInstance.children[0]?.depth !== 1) {
+    templateFile.message(new NoBigTitleInTemplateError(), { line: 0, column: 0 });
+  }
+  // 用状态机和栈把它搞成树
+  /** 保存之前经过的标题 */
+  const titleStack = [];
+  let currentHeadingDepth = 0;
+  /** 我们从模板中提取出的所有信息 */
+  const templateData: ITemplateData = {
+    outlines: [],
+    resources: {},
+    title: templateFile.basename ?? '',
+  };
+
+  for (let mdNodeIndex = 0; mdNodeIndex < mdastInstance.children.length; mdNodeIndex += 1) {
+    const currentMDASTNode = mdastInstance.children[mdNodeIndex];
+    const nextMDASTNode = mdastInstance.children[mdNodeIndex + 1] as Content | undefined;
+    switch (currentMDASTNode.type) {
+      case 'heading': {
+        // 首先标题得有内容，言之有物
+        let titleTextValue = '';
+        const titleTextNode = currentMDASTNode.children[0];
+        if (titleTextNode?.type === 'text' && titleTextNode.value.length > 0) {
+          titleTextValue = titleTextNode.value;
+        } else {
+          templateFile.message(new NoTitleContentError(), currentMDASTNode.position);
+        }
+        currentHeadingDepth = currentMDASTNode.depth;
+        // 处理大标题
+        if (currentMDASTNode.depth === 1) {
+          if (typeof titleTextValue === 'string' && titleTextValue.length > 0) {
+            templateData.title = titleTextValue;
+          }
+          // 大标题的下几个段落节点即是大纲节点
+          let foundOutlineNode = false;
+          for (let outlineNodeDetectorIndex = mdNodeIndex + 1; outlineNodeDetectorIndex < mdastInstance.children.length; outlineNodeDetectorIndex += 1) {
+            const nextOutlineNode = mdastInstance.children[outlineNodeDetectorIndex];
+            if (nextOutlineNode?.type === 'paragraph') {
+              /**
+             * ```json
+             * children: [
+                {
+                  type: 'text',
+                  value: '做得好的：项目·起\n做得好的：项目·承\n做得好的：项目·转\n做得好的：项目·合',
+                  position: [Object]
+                }
+              ]
+              ```
+             */
+              const outlineTextNode = nextOutlineNode.children[0];
+              if (outlineTextNode?.type === 'text') {
+                foundOutlineNode = true;
+                // 移动当前指针，这样解析完标题节点之后，我们的指针就跳过了大纲区域
+                mdNodeIndex += 1;
+                if (templateData.outlines === undefined) {
+                  templateData.outlines = [];
+                }
+                templateData.outlines.push(outlineTextNode.value.split('\n'));
+              }
+            } else {
+              // 遇到下一个标题时，跳出。此处默认我们要不就是 Heading 要不就是 Paragraph
+              break;
+            }
+          }
+          if (!foundOutlineNode) {
+            templateFile.message(new NoOutlineInTemplateError(), nextMDASTNode?.position ?? currentMDASTNode.position);
+          }
+          break;
+        } else {
+          // 仅对于二级以上标题建栈，因为二级以上标题才算是开始构建模板内容树
+          titleStack.push(titleTextValue);
+          // 判断一下现在是不是根节点了，根节点意味着后面没有更深层次的标题了
+          let rootNodeDetectorPassingText = false;
+          for (let rootNodeDetectorIndex = mdNodeIndex; rootNodeDetectorIndex < mdastInstance.children.length; rootNodeDetectorIndex += 1) {
+            const rootNodeDetectorCurrentMDASTNode = mdastInstance.children[rootNodeDetectorIndex];
+            // 只会有两种情况：文本或标题，如果是标题则判断一下，文本就都正常，不过要注意检测一下没有两个不同级标题先高后低夹着文本的情况，这个是不允许的
+            if (rootNodeDetectorCurrentMDASTNode.type === 'text') {
+              rootNodeDetectorPassingText = true;
+            } else if (rootNodeDetectorCurrentMDASTNode.type === 'heading') {
+              // 找到了同级节点或上级节点，说明本节点正常结束了，结束检测
+              if (rootNodeDetectorPassingText && rootNodeDetectorCurrentMDASTNode.depth === currentMDASTNode.depth) {
+                templateFile.message(new NoTextBetweenTitleError(), rootNodeDetectorCurrentMDASTNode.position);
+              }
+              if (rootNodeDetectorCurrentMDASTNode.depth >= currentMDASTNode.depth) {
+                break;
+              }
+              // 标题先高后低夹着文本，直接报错
+              if (rootNodeDetectorPassingText && rootNodeDetectorCurrentMDASTNode.depth < currentMDASTNode.depth) {
+                templateFile.fail(new BadTextBetweenTitleError(), rootNodeDetectorCurrentMDASTNode.position);
+              }
+            } else {
+              // 带上了其他奇奇怪怪的 Markdown 语法，报个错
+              templateFile.fail(new BadTextBetweenTitleError(), rootNodeDetectorCurrentMDASTNode.position);
+            }
+          }
+          break;
+        }
+      }
+      case 'paragraph': {
+        // 到达了根节点，准备往资源库里塞数据，构建一下数据路径
+        const resourcesDataPath = titleStack.join('.');
+        // 如果下一个节点是标题，这个标题要不就是和之前同级，要不就是更高级的（depth 更小的）（不会是更低级的（depth 更大的），上面的检测保证了），说明要出栈了
+        const nextHeadingNode = nextMDASTNode;
+        if (nextHeadingNode?.type === 'heading') {
+          for (let popTime = 0; popTime < currentHeadingDepth - nextHeadingNode.depth + 1; popTime += 1) {
+            titleStack.pop();
+          }
+        }
+        // 一个 Paragraph 里只会有一个 TextNode
+        const paragraphTextNode = currentMDASTNode.children[0];
+        if (paragraphTextNode?.type !== 'text' || paragraphTextNode.value.length === 0) {
+          templateFile.fail(new BadTextBetweenTitleError(), paragraphTextNode.position);
+        }
+        // 我们往 Paragraph 里塞 Sentence（每个自然段是一个 Sentence，因为它们最后就只是生成一句话），一个 Sentence 里会有多个 Word，每个就是一行文本，我们将一行文本看做是一个模因，所以称为 Word 也算合理。一行文本经过分词之后，每一个语素就用 Text 和 Symbol 来承装吧。当然，一个核心理由是，MDAST 里缺少 Sentence 这个级别的概念。
+        // 我们取出字符串内容，并去掉空行和空白
+        const textValueLines = paragraphTextNode.value.split('\n');
+        const unistWordMemeNodes: Word[] = textValueLines.map((line) => {
+          // 把模板变成 UNIST 节点
+          const memeFragmentNodes: TextNode[] = line
+            .split(/({{[^{}]+}})/g)
+            .flatMap((memePart) => memePart.split(/(\[\[[^[\]]+]])/g))
+            .map((memePart) =>
+              u('TextNode', {
+                value: memePart,
+                // 如果是{{正面工作}}这样的节点，则标注为待填的槽（slot 是我们自定义的元信息），等待之后替换为具体内容
+                slot: /({{[^{}]+}})/.test(memePart) ? memePart.replace(/[{}]/g, '') : undefined,
+                // 如果是[[目标感>1;目标感+1]]这样的节点，则标注为元信息表，等待之后提取
+                metadata: /(\[\[[^[\]]+]])/.test(memePart) ? memePart.replace(/[[\]]/g, '').split(/[,，]/g) : undefined,
+              }),
+            );
+          return u('WordNode', { children: memeFragmentNodes });
+        });
+        const unistSentenceNode: Sentence = u('SentenceNode', { children: unistWordMemeNodes });
+        // 塞数据了
+        const previousParagraphNode = get(templateData.resources, resourcesDataPath) ?? u('ParagraphNode', { children: [] as Sentence[] });
+        if (!Array.isArray(previousParagraphNode.children)) {
+          throw new TypeError(`程序逻辑漏洞：previousParagraphNode.children is not an Array： ${JSON.stringify(previousParagraphNode)}`);
+        }
+        previousParagraphNode.children.push(unistSentenceNode);
+        set(templateData.resources, resourcesDataPath, previousParagraphNode);
+      }
+    }
+  }
+  return templateData;
+}
